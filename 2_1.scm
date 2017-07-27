48ff9e8831852df57e9f7a44235704523fe98907
WindChimeRan <1012869439@qq.com>
2017-07-27 15:07:01 +0800

    2.1

---
+++ 2_1.scm
@@ -0,0 +1,18 @@
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (* (/ n g) (/ (* n d) (abs (* n d)))) (abs (/ d g)))))
+  
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(print-rat (make-rat 2 -4))
